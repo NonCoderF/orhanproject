fd57f363a517b48823c9907de512f80df21636a2
Nizam <nizamuddin.ahmed@vantagecircle.com>
2022-09-18 21:55:58 +0530

    t

--- src/main/kotlin/com/orhan/controllers/MainController.kt
+++ src/main/kotlin/com/orhan/controllers/MainController.kt
@@ -17,7 +17,6 @@ import org.json.JSONObject
 class MainController() {
 
     private val members = ConcurrentHashMap<String, Member>()
-    val responderCoroutine = CoroutineScope(Dispatchers.IO)
 
     fun onJoin(
         userId: String,
@@ -44,23 +43,21 @@ class MainController() {
 
             if (member.userId in directive.receivers) {
 
-                responderCoroutine.launch {
-                    while (true){
-                        val response: HttpResponse = HttpClient(CIO).get(
-                            "http://stock-rock-007.herokuapp.com?ticker=SBIN.NS?interval=1d&period=1m"
-                        )
+                while (true){
+                    val response: HttpResponse = HttpClient(CIO).get(
+                        "http://stock-rock-007.herokuapp.com?ticker=SBIN.NS?interval=1d&period=1m"
+                    )
 
-                        val responseString = response.content.readUTF8Line(response.content.availableForRead).toString()
+                    val responseString = response.content.readUTF8Line(response.content.availableForRead).toString()
 
-                        val json = JSONObject(responseString)
+                    val json = JSONObject(responseString)
 
-                        val responseJSON= JSONObject()
-                        responseJSON.put("timeStamp", System.currentTimeMillis())
-                        responseJSON.put("price", json.getJSONObject("Close"))
+                    val responseJSON= JSONObject()
+                    responseJSON.put("timeStamp", System.currentTimeMillis())
+                    responseJSON.put("price", json.getJSONObject("Close"))
 
-                        member.socket.send(Frame.Text(responseJSON.toString()))
-                        delay(1000)
-                    }
+                    member.socket.send(Frame.Text(responseJSON.toString()))
+                    Thread.sleep(1000)
                 }
 
             }
